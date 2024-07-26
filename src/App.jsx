55f7f4c894445a40b2309092e5f0ff3c1fcbf75b
ankitsensei <main.ankitbhagat@gmail.com>
2024-07-26 09:42:04 +0530

    added images for slideshow

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from './NavBar.jsx';
+import Page1 from './Page1.jsx';
 
 function App() {
     return (
         <div className='bg-zinc-900 w-full h-screen text-zinc-50'>
             <NavBar />
+            <Page1 />
         </div>
     );
 }
